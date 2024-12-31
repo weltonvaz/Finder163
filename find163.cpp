436871829dc3cf2d4e627cece485d21c882c8a19
Juliano <59809207+juliano-xd@users.noreply.github.com>
2024-12-30 22:39:25 -0300

    Adição de novos modos

--- find163.cpp
+++ find163.cpp
@@ -16,12 +16,14 @@ Modified by Trindade
 
 #include "secp256k1/SECP256k1.h"
 #include "secp256k1/Point.h"
-#include "secp256k1/Int.h"
 #include "hash/sha256.h"
 
+#define printhex(a) printf("%lx\n",a); // impressao de numeros hexadecimal facilitada
+#define printdec(a) printf("%ld\n",a); // impressao de numeros decimais facilitada
+
 Secp256K1 *secp = new Secp256K1();
-Int *priv = new Int();
 Point pubkey;
+uint64_t progress = 0x0;
 
 static const uint8_t target[20] = { // ripemd-160 cru da 163
     0x03,0x2d,0xdf,0x76,0xd2,
@@ -30,77 +32,130 @@ static const uint8_t target[20] = { // ripemd-160 cru da 163
     0x25,0x1a,0x65,0x48,0xdc
 };
 
-
-static uint8_t mutables[] = { // encontradas: 18 (letras 15 e 16 nao vao ser reveladas)
-    0xf3,0x35,0xa0,0xcf,0x57,0x0e,0x40,0xb0,// essas duas ultimas nao serao reveladas por ele
-    0xb1,0x7b,0x2a,0x60,0x86,0x70,0x86,0xa0,
-    0x40,0x00,0x80,0x30,0x30,0x30,0x70,0x30,
+// 15,16,20,22,24,25,26,27,28,29,30,31,32
+uint8_t mutables[] = {// esses sao os bytes que ainda nao foram encontrados
+    0x40,0xb0,0x60,0x70,0xa0,0x40,0x00,0x80,0x30,0x30,0x30,0x70,0x30
 };
+
 static uint8_t pub[33];
 
 void Init(){
-    priv->SetQWord(3, 0x403b3d4fcff56a92);// valor fixo
+    secp->Init();
     printf("%s\n","find 163 by Trindade v1\n Esse codigo é uma modificação do codigo do keyhunt");
     printf("Esse codigo faz a busca da chave privada que gera o rimpad da 163\n que é esse: 032ddf76d2ad152cb5b391bfba3d24251a6548dc ");
+    printf("Carteira para doações: bc1qlywys40f6p3njax5txf8lh4qs2hmt58nmsp5j7");
     printf("\nProcurando...\n");
-    secp->Init();
 }
 
-uint8_t proximidade = 0; // conta quantos bytes foram iguais ao target na comparação
 bool compare(){
-    for (uint8_t i = 0; i < 20; i++) { // primeiro byte sempre igual
-        if(pub[i] != target[i]) return false;
-        proximidade = i;
-    }
-    return true;
+    uint8_t i = 19;
+    while (pub[i] == target[i] && i--);
+    return i == 0xff;
 }
 
-unsigned char buf[14];  // Buffer para armazenar os 14 bytes aleatórios
+const uint8_t f0 = 0xf0;
+void zerate(){
+    mutables[0] &= f0;
+    mutables[1] &= f0;
+    mutables[2] &= f0;
+    mutables[3] &= f0;
+    mutables[4] &= f0;
+    mutables[5] &= f0;
+    mutables[6] &= f0;
+    mutables[7] &= f0;
+    mutables[8] &= f0;
+    mutables[9] &= f0;
+    mutables[10] &= f0;
+    mutables[11] &= f0;
+    mutables[12] &= f0;
+}
+
+uint8_t *bP = (uint8_t*)&progress;
+uint8_t getL4BP(uint8_t index){// pega os 4 bits baixos do progress
+    return bP[index] & 0x0f;
+}
+uint8_t getH4BP(uint8_t index){// pega os 4 bits altos do progress
+    return (bP[index] & 0xf0) >> 4;
+}
+
 void randomize(){
-    getrandom(buf, sizeof(buf), GRND_NONBLOCK);
-    mutables[6] |= buf[0]>>4;
-    mutables[7] |= buf[1]>>4;
-    mutables[11] |= buf[2]>>4;
-    mutables[13] |= buf[3]>>4;
-    mutables[15] |= buf[5]>>4;
-    mutables[16] |= buf[6]>>4;
-    mutables[17] |= buf[7]>>4;
-    mutables[18] |= buf[8]>>4;
-    mutables[19] |= buf[9]>>4;
-    mutables[20] |= buf[10]>>4;
-    mutables[21] |= buf[11]>>4;
-    mutables[22] |= buf[12]>>4;
-    mutables[23] |= buf[13]>>4;
+    zerate();
+    getrandom(&progress, 8, GRND_NONBLOCK);
+    mutables[12] |= getL4BP(0);
+    mutables[11] |= getH4BP(0);
+    mutables[10] |= getL4BP(1);
+    mutables[9]  |= getH4BP(1);
+    mutables[8] |= getL4BP(2);
+    mutables[7]  |= getH4BP(2);
+    mutables[6] |= getL4BP(3);
+    mutables[5]  |= getH4BP(3);
+    mutables[4] |= getL4BP(4);
+    mutables[3]  |= getH4BP(4);
+    mutables[2] |= getL4BP(5);
+    mutables[1]  |= getH4BP(5);
+    mutables[0] |= getL4BP(6);
+}
+void increment(){
+    zerate();
+    progress++;
+    mutables[12] |= getL4BP(0);
+    mutables[11] |= getH4BP(0);
+    mutables[10] |= getL4BP(1);
+    mutables[9]  |= getH4BP(1);
+    mutables[8] |= getL4BP(2);
+    mutables[7]  |= getH4BP(2);
+    mutables[6] |= getL4BP(3);
+    mutables[5]  |= getH4BP(3);
+    mutables[4] |= getL4BP(4);
+    mutables[3]  |= getH4BP(4);
+    mutables[2] |= getL4BP(5);
+    mutables[1]  |= getH4BP(5);
+    mutables[0] |= getL4BP(6);
+}
+void decrement(){
+    zerate();
+    progress--;
+    mutables[0] |= getL4BP(0);
+    mutables[1] |= getH4BP(0);
+    mutables[2] |= getL4BP(1);
+    mutables[3]  |= getH4BP(1);
+    mutables[4] |= getL4BP(2);
+    mutables[5]  |= getH4BP(2);
+    mutables[6] |= getL4BP(3);
+    mutables[7]  |= getH4BP(3);
+    mutables[8] |= getL4BP(4);
+    mutables[9]  |= getH4BP(4);
+    mutables[10] |= getL4BP(5);
+    mutables[11]  |= getH4BP(5);
+    mutables[12] |= getL4BP(6);
 }
 
 void printArray(uint8_t *arr, size_t size){
-    for (size_t i = 0; i < size; i++) {
-        printf("%02x",arr[i]);
-    }
-    printf("\n");
+    uint8_t i = 0;
+    while (i < size) printf("%02x", arr[i++]);
 }
 
 int main(){
     Init();
-    uint8_t maisProximo = 0;
-    printf("Mais proximo encontrado:\n");
 
-    inform: printf("\rBytes iguais %d:",maisProximo);
-    start: randomize();
-    priv->SetLower24Bytes((uint64_t*)mutables);
-    pubkey = secp->ComputePublicKey(priv);
-    secp->GetPubKeyHexCompressed(pubkey, pub);
-    sha256_33(pub, pub);
-    RIPEMD160(pub, 32, pub);
-
-    if (compare()){
-        printf("\nEncontrou!! %s\n", priv->GetBase16());
-        return 0;
+    while (true) {
+        // Esses sao os modos atuais, basta descomentar e testar.
+        // Nenhum modo pode ser misturado com o outro ainda, um sobrepoe o outro se tentar.
+        // randomize();
+        // increment();
+        // decrement();
+        pubkey = secp->OptimizationPubKeyComp(mutables);
+        secp->GetPubKeyHexCompressed(pubkey, pub);
+        sha256_33(pub, pub);
+        RIPEMD160(pub, 32, pub);
+        if (compare()){
+            printf("\nEncontrou!!\n");
+            printf("Esses sao os bytes que estavam faltando:\n");
+            printArray(mutables, 13);
+            return 0;
+        }
+        // para mostrar os bytes mudando no terminal, basta descomentar essa linha de baixo
+        printArray(mutables, 13); printf("\r");
     }
-    if (maisProximo < proximidade) {
-        maisProximo = proximidade;
-        goto inform;
-    }
-    goto start;
     return 0;
 }
